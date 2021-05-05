c81076a6d0e07fe6b68378915553d3b33fd733af
goatmxj67 <yulki67@naver.com>
2021-05-05 21:55:36 +0900

    update 2021-05-05

---
+++ pythonworkspace/02/02_5_주석.py
@@ -0,0 +1,19 @@
+# 애완동물을 소개해 주세요~
+animal = "고양이"
+name = "해피"
+age = 4
+hobby = "낮잠"
+is_adult = age >= 3
+
+''' 이렇게 
+하면
+여러문장이
+주석처리가 됩니다 '''
+
+# ctrl + / = 주석 (블록으로 가능)
+# print("우리집 " + animal + "의 이름은 " + name + "에요")
+# hobby = "공놀이"
+# #print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+# print(name + "는 어른일까요? " + str(is_adult))
+
