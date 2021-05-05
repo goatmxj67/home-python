f1b7ff5f6f4a4f5db2080e31f0a64f2be916a51c
goatmxj67 <yulki67@naver.com>
2021-05-05 23:44:12 +0900

    update 2021-05-05

---
+++ pythonworkspace/04/04_3_문자열처리함수.py
@@ -0,0 +1,17 @@
+python = "Python is Amazing"
+print(python.lower())
+print(python.upper())
+print(python[0].isupper())
+print(len(python))
+print(python.replace("Python", "Java"))
+
+index = python.index("n")
+print(index)
+index = python.index("n", index + 1) # index + 1 부터 찾아준다.
+print(index)
+
+print(python.find("Java")) # 못 찾으면 -1 반환 / 찾으면 0
+#print(python.index("Java")) # 못 찾으면 오류 -> 다음 줄 부터 실행 안함
+print("hi")
+
+print(python.count("n"))
